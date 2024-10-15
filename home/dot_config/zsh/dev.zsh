8fd8a2c69deab958ffba5acbfea7cccb4e559a54
Phillip Simons <phillip@book.io>
2024-10-15 13:38:53 -0500

    Update home/.config/zsh/dev.zsh

--- home/dot_config/zsh/dev.zsh
+++ home/dot_config/zsh/dev.zsh
@@ -1,26 +1,4 @@
 . "$HOME/.asdf/asdf.sh"
-PATH="/home/phillip/perl5/bin${PATH:+:${PATH}}"; export PATH;
-PERL5LIB="/home/phillip/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="/home/phillip/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"/home/phillip/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/home/phillip/perl5"; export PERL_MM_OPT;
-# cargo bin
-export PATH="$PATH:$HOME/.cargo/bin"
-# sccache settings
-export SCCACHE_CACHE_SIZE=100G
-export CMAKE_C_COMPILER_LAUNCHER=sccache
-export CMAKE_CXX_COMPILER_LAUNCHER=sccache
-
 
 # set up NVM
 source /usr/share/nvm/init-nvm.sh
-
-export PATH="$PATH:$HOME/.ghcup/bin"
-export PATH="$PATH:$HOME/.nix-profile/bin"
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PATH="$PATH:$HOME/.ghcup/bin"
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
-export DIRENV_LOG_FORMAT=
-eval "$(direnv hook zsh)"
