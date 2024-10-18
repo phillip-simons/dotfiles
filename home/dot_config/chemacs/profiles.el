c26eb1c89524cbc24bb0d090d795a18be165fe9e
Phillip Simons <phillip@book.io>
2024-10-18 11:55:04 -0500

    Add home/.config/chemacs/profile Add home/.config/chemacs/profiles.el

---
+++ home/dot_config/chemacs/profiles.el
@@ -0,0 +1,8 @@
+(
+ ("spacemacs" . ((user-emacs-directory . "~/.config/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.config/spacemacs.d")))))
+ ("doom" . ((user-emacs-directory . "~/.config/doom-emacs")
+            (server-name . "doom")
+            (env . (("DOOMDIR" . "~/.config/doom.d")))
+            ))
+ )
